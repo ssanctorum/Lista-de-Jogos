0f13d7a95c071c6bfc714aa760afcc7b05f812f4
Warlley Kaio dos Santos Firmino <warlley.firmino@nazaria.com.br>
2026-05-22 18:08:34 -0300

    retirei tudo que era do JOptionPane da classe, falta apenas saber como retornar o item excluido

--- src/PilhaConsoles.java
+++ src/PilhaConsoles.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-
 public class PilhaConsoles {
 
     private Console[] pilhaDeConsoles = new Console[5];
@@ -9,49 +7,65 @@ public class PilhaConsoles {
         return topoDosConsoles;
     }
 
-    public void pilhaPush(Console console){
+    public void pilhaPush(Console console) {
         pilhaDeConsoles[topoDosConsoles] = console;
         topoDosConsoles++;
 
-        if (pilhaDeConsoles.length == topoDosConsoles){
-            Console[] novaPilhaDeConsoles = new Console [pilhaDeConsoles.length * 2];
+        if (pilhaDeConsoles.length == topoDosConsoles) {
+            Console[] novaPilhaDeConsoles = new Console[pilhaDeConsoles.length * 2];
 
-            for (int i = 0; i < pilhaDeConsoles.length; i++){
-                novaPilhaDeConsoles[i] = pilhaDeConsoles [i];
+            for (int i = 0; i < pilhaDeConsoles.length; i++) {
+                novaPilhaDeConsoles[i] = pilhaDeConsoles[i];
             }
 
             pilhaDeConsoles = novaPilhaDeConsoles;
         }
     }
 
-    public void pilhaPop(){
+    ///falta apenas tentar retornar o console que foi excluido
+    public void pilhaPop() {
+        Console console = pilhaDeConsoles[topoDosConsoles];
+        String string = "";
+
+//        console.toString() = string;
+        pilhaDeConsoles[topoDosConsoles - 1] = null;
+        topoDosConsoles--;
 
 
-        pilhaDeConsoles[topoDosConsoles-1] = null;
+
+
     }
 
-    /// RETIRAR QUAISQUER RESQUICIOS DO JOPTIONPANE, ESSE AQUI NAO ESTA FUNCIONANDO DIREITO; TALVEZ UM FOR NO METODOS DE CERTO
-//    public String pilhaListar(){
-//        String consolesRetorno = "";
-//
-//        for (int i = topoDosConsoles-1; i >= 0; i--){
-//
-//            Console console = pilhaDeConsoles[i];
-//            consolesRetorno += console.toString();
-//        }
-//        return consolesRetorno;
-//    }
+    public String pilhaListar() {
+        String consolesRetorno = "";
 
-    /// RETIRAR QUAISQUER RESQUICIOS DO JOPTIONPANE
-//    public void pilhaBuscar(String string){
-//
-//        for (int i = 0; i < topoDosConsoles; i++){
-//
-//            Console console = pilhaDeConsoles[i];
-//
-//            if (string.equalsIgnoreCase(console.getNome())){
-//                JOptionPane.showMessageDialog(null, console.toString(), "Buscar console", JOptionPane.PLAIN_MESSAGE);
-//            }
-//        }
-//    }
+        for (int i = topoDosConsoles - 1; i >= 0; i--) {
+
+            Console console = pilhaDeConsoles[i];
+            consolesRetorno += console.toString() + "\n\n";
+        }
+        return consolesRetorno;
+    }
+
+
+    public String pilhaBuscar(String stringBusca) {
+
+        String stringRetorno = "";
+
+        for (int i = topoDosConsoles - 1; i >= 0 ; i--) {
+
+            Console console = pilhaDeConsoles[i];
+
+            if (console.getNome().toLowerCase().contains(stringBusca.toLowerCase())) {
+                stringRetorno += console.toString() + "\n\n";
+
+            }
+        }
+
+        if (stringRetorno == ""){
+            return "Nenhum console: " + stringBusca + " encontrado.";
+        }
+
+        return stringRetorno;
+    }
 }
